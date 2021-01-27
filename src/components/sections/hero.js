c6948a4a747dea9da083165e912c57da500198de
Subhaprada Chand <subhapradachand@gmail.com>
2021-01-27 17:12:08 +0530

    Updated hero section

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -48,12 +48,12 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
+  const two = <h2 className="big-heading">Subhaprada Chand.</h2>;
   const three = <h3 className="big-heading">I build things for the web.</h3>;
   const four = (
     <p>
-      I'm a software engineer based in Boston, MA specializing in building (and occasionally
-      designing) exceptional websites, applications, and everything in between.
+      I'm a software engineer based in Bangalore, Karnataka specializing in building exceptional
+      websites and highly scalable and fault-tolerant systems.
     </p>
   );
   const five = (
