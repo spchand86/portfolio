679927c7e4e2bd36bbbf2601d1318606af956268
Subhaprada Chand <subhapradachand@gmail.com>
2021-01-27 17:12:30 +0530

    Updated about section

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -132,7 +132,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'HTML & (S)CSS', 'React', 'Vue', 'Node.js', 'WordPress'];
+  const skills = ['Java', 'Spring Boot', 'JavaScript (ES6+)', 'HTML & (S)CSS', 'React', 'C++'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -141,19 +141,26 @@ const About = () => {
       <div className="inner">
         <StyledText>
           <div>
-            <p>Hello! I'm Brittany, a software engineer based in Boston, MA.</p>
+            <p>Hello! I'm Subhaprada, a software engineer based in Bangalore, Karnataka.</p>
 
-            <p>
-              I enjoy creating things that live on the internet, whether that be websites,
-              applications, or anything in between. My goal is to always build products that provide
-              pixel-perfect, performant experiences.
-            </p>
+            <p>I enjoy creating highly scalable, robust backend systems.</p>
 
             <p>
               Shortly after graduating from{' '}
-              <a href="https://www.ccis.northeastern.edu">Northeastern University</a>, I joined the
-              engineering team at <a href="https://www.upstatement.com">Upstatement</a> where I work
-              on a wide variety of interesting and meaningful projects on a daily basis.
+              <a href="https://www.bits-pilani.ac.in/">BITS Pilani in 2019</a>, I joined the
+              engineering team at <a href="https://www.razorpay.com">Razorpay</a> where I work on a
+              wide variety of interesting and meaningful projects on a daily basis as an automation
+              engineer.
+            </p>
+
+            <p>
+              I have since left Razorpay and am actively looking at opportunities in backend
+              development.
+            </p>
+
+            <p>
+              I maintain a blog here where I write about the tech problems that I have been working
+              on recently.
             </p>
 
             <p>Here are a few technologies I've been working with recently:</p>
