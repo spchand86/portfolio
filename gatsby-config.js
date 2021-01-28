4e842732d8edb7495f6ad754d01e2b4338bcafe9
Subhaprada Chand <subhapradachand@gmail.com>
2021-01-28 17:55:32 +0530

    Made changes to site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,10 +7,10 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Subhaprada Chand',
     description:
-      'Brittany Chiang is a software engineer specializing in building (and occasionally designing) exceptional websites, applications, and everything in between.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Subhaprada Chand is a software engineer specializing in building exceptional websites and highly scalable and fault-tolerant systems.',
+    siteUrl: 'https://subhapradachand.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
