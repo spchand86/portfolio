f15f35214c81955d747d13e54c7b29b40e5aabd7
Subhaprada Chand <subhapradachand@gmail.com>
2021-01-29 21:43:18 +0530

    Modified post component

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { graphql, Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
+import parse from 'html-react-parser';
+import Image from 'gatsby-image';
 import kebabCase from 'lodash/kebabCase';
-import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import styled from 'styled-components';
 import { Layout } from '@components';
+import SEO from '../components/seo';
+import PropTypes from 'prop-types';
 
 const StyledPostContainer = styled.main`
   max-width: 1000px;
@@ -34,8 +37,13 @@ const StyledPostContent = styled.div`
 `;
 
 const PostTemplate = ({ data, location }) => {
-  const { frontmatter, html } = data.markdownRemark;
-  const { title, date, tags } = frontmatter;
+  const { previous, next, post } = data;
+  const { title, date, tags } = post;
+  const { tagNodes } = tags;
+  const featuredImage = {
+    fluid: post.featuredImage?.node?.localFile?.childImageSharp?.fluid,
+    alt: post.featuredImage?.node?.alt || ``,
+  };
 
   return (
     <Layout location={location}>
@@ -44,7 +52,7 @@ const PostTemplate = ({ data, location }) => {
       <StyledPostContainer>
         <span className="breadcrumb">
           <span className="arrow">&larr;</span>
-          <Link to="/pensieve">All memories</Link>
+          <Link to="/pensieve">All Blog posts</Link>
         </span>
 
         <StyledPostHeader>
@@ -58,17 +66,61 @@ const PostTemplate = ({ data, location }) => {
               })}
             </time>
             <span>&nbsp;&mdash;&nbsp;</span>
-            {tags &&
-              tags.length > 0 &&
-              tags.map((tag, i) => (
-                <Link key={i} to={`/pensieve/tags/${kebabCase(tag)}/`} className="tag">
-                  #{tag}
+            {tagNodes &&
+              tagNodes.length > 0 &&
+              tagNodes.map((tag, i) => (
+                <Link key={i} to={`/pensieve/tags/${kebabCase(tag.name)}/`} className="tag">
+                  #{tag.name}
                 </Link>
               ))}
           </p>
         </StyledPostHeader>
 
-        <StyledPostContent dangerouslySetInnerHTML={{ __html: html }} />
+        <SEO title={post.title} description={post.excerpt} />
+
+        <article className="blog-post" itemScope itemType="http://schema.org/Article">
+          <header>
+            {featuredImage?.fluid && (
+              <Image
+                fluid={featuredImage.fluid}
+                alt={featuredImage.alt}
+                style={{ marginBottom: 20 }}
+              />
+            )}
+          </header>
+          <StyledPostContent>
+            {!!post.content && <section itemProp="articleBody">{parse(post.content)}</section>}
+          </StyledPostContent>
+
+          <hr />
+        </article>
+
+        <nav className="blog-post-nav">
+          <ul
+            style={{
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `space-between`,
+              listStyle: `none`,
+              padding: 0,
+            }}>
+            <li>
+              {previous && (
+                <Link to={previous.uri} rel="prev">
+                  ← {parse(previous.title)}
+                </Link>
+              )}
+            </li>
+
+            <li>
+              {next && (
+                <Link to={next.uri} rel="next">
+                  {parse(next.title)} →
+                </Link>
+              )}
+            </li>
+          </ul>
+        </nav>
       </StyledPostContainer>
     </Layout>
   );
@@ -77,21 +129,77 @@ const PostTemplate = ({ data, location }) => {
 export default PostTemplate;
 
 PostTemplate.propTypes = {
-  data: PropTypes.object,
+  data: PropTypes.shape({
+    previous: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      uri: PropTypes.string.isRequired,
+    }),
+    next: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      uri: PropTypes.string.isRequired,
+    }),
+    post: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      date: PropTypes.instanceOf(Date).isRequired,
+      excerpt: PropTypes.string.isRequired,
+      content: PropTypes.string.isRequired,
+      featuredImage: PropTypes.object,
+
+      tags: PropTypes.shape({
+        tagNodes: PropTypes.shape({
+          name: PropTypes.string.isRequired,
+          length: PropTypes.number.isRequired,
+          map: PropTypes.object,
+        }),
+      }),
+    }),
+  }),
   location: PropTypes.object,
 };
 
 export const pageQuery = graphql`
-  query($path: String!) {
-    markdownRemark(frontmatter: { slug: { eq: $path } }) {
-      html
-      frontmatter {
-        title
-        description
-        date
-        slug
-        tags
+  query BlogPostById(
+    # these variables are passed in via createPage.pageContext in gatsby-node.js
+    $id: String!
+    $previousPostId: String
+    $nextPostId: String
+  ) {
+    # selecting the current post by id
+    post: wpPost(id: { eq: $id }) {
+      id
+      excerpt
+      content
+      title
+      date(formatString: "MMMM DD, YYYY")
+      tags {
+        tagNodes: nodes {
+          name
+        }
       }
+      featuredImage {
+        node {
+          altText
+          localFile {
+            childImageSharp {
+              fluid(maxWidth: 1000, quality: 100) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+
+    # this gets us the previous post by id (if it exists)
+    previous: wpPost(id: { eq: $previousPostId }) {
+      uri
+      title
+    }
+
+    # this gets us the next post by id (if it exists)
+    next: wpPost(id: { eq: $nextPostId }) {
+      uri
+      title
     }
   }
 `;
